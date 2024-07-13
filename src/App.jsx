1c2a41386b3c84010e56f884c9a5c20e9ef4b672
kasimugur <kasiugur7@gmail.com>
2024-07-13 18:44:44 +0300

    pages and components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Dashboard from './components/Dashboard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>hello</h1>
+    <Dashboard />
     </>
   )
 }
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,73 @@
+export default function Dashboard() {
+  return (
+    <>
+      <div className="container">
+        <div className="card">
+          <div className="header">
+            <img className="img-prf" src="../images/image-jeremy.png" alt="profilo" />
+            <span>Report for</span>
+            <h1>Jeremy Robson</h1>
+          </div>
+          <a href="">Daily</a>
+          <a href="">Weekly</a>
+          <a href="">Monthly</a>
+        </div>
+        <div className="card-item">
+          <div className="info">
+            <div className="head">
+              <h4>Work</h4><span>...</span>
+            </div>
+            <h1>32hrs</h1>
+            <span>Last Week - 36hrs</span>
+          </div>
+        </div>
+        <div className="card-item">
+          <div className="info">
+            <div className="head">
+              <h4>Work</h4><span>...</span>
+            </div>
+            <h1>32hrs</h1>
+            <span>Last Week - 36hrs</span>
+
+          </div>
+        </div>
+        <div className="card-item">
+          <div className="info">
+            <div className="head">
+              <h4>Work</h4><span>...</span>
+            </div>
+            <h1>32hrs</h1>
+            <span>Last Week - 36hrs</span>
+          </div>
+        </div>
+        <div className="card-item">
+          <div className="info">
+            <div className="head">
+              <h4>Work</h4><span>...</span>
+            </div>
+            <h1>32hrs</h1>
+            <span>Last Week - 36hrs</span>
+          </div>
+        </div>
+        <div className="card-item">
+          <div className="info">
+            <div className="head">
+              <h4>Work</h4><span>...</span>
+            </div>
+            <h1>32hrs</h1>
+            <span>Last Week - 36hrs</span>
+          </div>
+        </div>
+        <div className="card-item">
+          <div className="info">
+            <div className="head">
+              <h4>Work</h4><span>...</span>
+            </div>
+            <h1>32hrs</h1>
+            <span>Last Week - 36hrs</span>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
