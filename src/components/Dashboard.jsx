042081d37d324338861ed1f29d94f5c66eaaf10e
kasimugur <kasiugur7@gmail.com>
2024-07-15 15:56:34 +0300

    scss added

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,20 +1,19 @@
 import { useState } from 'react'
 import '../css/style.css'
-import data from'../data.json'
-import { iconEllipsis, iconExercise } from '../pages/Page'
+import data from '../data.json'
+import { iconEllipsis } from '../pages/Page'
 export default function Dashboard() {
   const [daily, setDaily] = useState(null)
-  const [weekly, setWeekly] = useState(data.map(e => e.timeframes.weekly))
+  const [weekly, setWeekly] = useState(null)
   const [monthly, setMonthly] = useState(null)
-  
-console.log(daily)
-console.log(weekly)
-console.log(monthly)
-return (
+  const [dates, setDates] = useState()
+
+
+  return (
     <>
       <div className="container">
         <div className="card" >
-          <div className="header" > 
+          <div className="header" >
             <img className="img-prf" src="src/images/image-jeremy.png" alt="profilo" />
             <span>Report for</span>
             <h1>Jeremy Robson</h1>
@@ -24,59 +23,64 @@ return (
           <a onClick={() => setMonthly(data.map(e => e.timeframes.monthly))} href="">Monthly</a>
         </div>
         <div className="card-item">
-          <div className="info">
-            <div className="head">
-              <h4>work</h4> <span>{iconEllipsis} </span>
+
+
+          <div className="card-work">
+            <div className="info">
+              <div className="head">
+                <h4>Work</h4> <span>{iconEllipsis} </span>
+              </div>
+              <h1>32hrs</h1>
+              <span>Last Week - 36hrs</span>
             </div>
-            <h1>32hrs</h1>
-            <span>Last Week - 36hrs</span>
+          </div>
+          <div className="card-play">
+            <div className="info">
+              <div className="head">
+                <h4>Play</h4><span>{iconEllipsis} </span>
+              </div>
+              <h1>32hrs</h1>
+              <span>Last Week - 36hrs</span>
+            </div>
+          </div>
+          <div className="card-study">
+            <div className="info">
+              <div className="head">
+                <h4>Play</h4><span>{iconEllipsis} </span>
+              </div>
+              <h1>32hrs</h1>
+              <span>Last Week - 36hrs</span>
+            </div>
+          </div>
+          <div className="card-exercise">
+            <div className="info">
+              <div className="head">
+                <h4>Play</h4><span>{iconEllipsis} </span>
+              </div>
+              <h1>32hrs</h1>
+              <span>Last Week - 36hrs</span>
+            </div>
+          </div>
+          <div className="card-social">
+            <div className="info">
+              <div className="head">
+                <h4>Play</h4><span>{iconEllipsis} </span>
+              </div>
+              <h1>32hrs</h1>
+              <span>Last Week - 36hrs</span>
+            </div>
+          </div>
+          <div className="card-selfcare">
+            <div className="info">
+              <div className="head">
+                <h4>Play</h4><span>{iconEllipsis} </span>
+              </div>
+              <h1>32hrs</h1>
+              <span>Last Week - 36hrs</span>
+            </div>
           </div>
         </div>
-        <div className="card-item">
-          <div className="info">
-            <div className="head">
-              <h4>Work</h4><span>{iconEllipsis} </span>
-            </div>
-            <h1>32hrs</h1>
-            <span>Last Week - 36hrs</span>
-          </div>
-        </div>
-        <div className="card-item">
-          <div className="info">
-            <div className="head">
-              <h4>Work</h4><span>{iconEllipsis} </span>
-            </div>
-            <h1>32hrs</h1>
-            <span>Last Week - 36hrs</span>
-          </div>
-        </div>
-        <div className="card-item">
-          <div className="info">
-            <div className="head">
-              <h4>Work</h4><span>{iconEllipsis} </span>
-            </div>
-            <h1>32hrs</h1>
-            <span>Last Week - 36hrs</span>
-          </div>
-        </div>
-        <div className="card-item">
-          <div className="info">
-            <div className="head">
-              <h4>Work</h4><span>{iconEllipsis} </span>
-            </div>
-            <h1>32hrs</h1>
-            <span>Last Week - 36hrs</span>
-          </div>
-        </div>
-        <div className="card-item">
-          <div className="info">
-            <div className="head">
-              <h4>Work</h4><span>{iconEllipsis} </span>
-            </div>
-            <h1>32hrs</h1>
-            <span>Last Week - 36hrs</span>
-          </div>
-        </div>
+
       </div>
     </>
   )
