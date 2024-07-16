90b5d3aab5de1bdcd174ded00d3bc20e02a47b73
kasimugur <kasiugur7@gmail.com>
2024-07-16 13:53:21 +0300

    images added

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from 'react'
 import '../css/style.css'
-import { iconEllipsis } from '../pages/Page'
+import { iconEllipsis, iconExercise, iconPlay, iconSelfCare, iconSocial, iconStudy, iconWork } from '../pages/Page'
 export default function Dashboard() {
-  const [daily, setDaily] = useState("daily")
-  const [weekly, setWeekly] = useState(null)
-  const [monthly, setMonthly] = useState(null)
   const [dates, setDates] = useState("weekly")
   const [data, setData] = useState([])
 
@@ -12,10 +9,6 @@ export default function Dashboard() {
     fetch('src/data.json').then(res => res.json()).then(data => setData(data))
       .catch(error => console.error('Error fetching data:', error))
   }, [])
-  const addClick = () => {
-  }
-  console.log(dates)
-  // console.log(data.map(e => e.title === "Work" && e.timeframes[dates].current))
   return (
     <>
       <div className="container" >
@@ -25,65 +18,73 @@ export default function Dashboard() {
             <span>Report for</span>
             <h1>Jeremy Robson</h1>
           </div >
+          <div className="btn">
           <button onClick={() => setDates("daily")} href="">Daily</button>
           <button onClick={() => setDates("weekly")} href="">Weekly</button>
           <button onClick={() => setDates("monthly")} href="">Monthly</button>
+          </div>
         </div>
         <div className="card-item">
 
 
           <div className="card-work">
+            {iconWork}
             <div className="info">
               <div className="head">
                 <h4>Work </h4> <span>{iconEllipsis} </span>
               </div>
-              <h1>{data.map(e => e.title === "Work" && e.timeframes.weekly.current)}hrs </h1>
-              <span>Last Week - {data.map(e => e.title === "Work" && e.timeframes.weekly.previous)}hrs</span>
+              <h1>{data.map(e => e.title === "Work" && e.timeframes[dates].current)}hrs </h1>
+              <span>Last Week - {data.map(e => e.title === "Work" && e.timeframes[dates].previous)}hrs</span>
             </div>
           </div>
           <div className="card-play">
+            {iconPlay}
             <div className="info">
               <div className="head">
                 <h4>Play</h4><span>{iconEllipsis} </span>
               </div>
-              <h1>{data.map(e => e.title === "Play" && e.timeframes.weekly.current)}hrs</h1>
-              <span>Last Week - {data.map(e => e.title === "Play" && e.timeframes.weekly.previous)}hrs</span>
+              <h1>{data.map(e => e.title === "Play" && e.timeframes[dates].current)}hrs</h1>
+              <span>Last Week - {data.map(e => e.title === "Play" && e.timeframes[dates].previous)}hrs</span>
             </div>
           </div>
           <div className="card-study">
+            {iconStudy}
             <div className="info">
               <div className="head">
                 <h4>Study</h4><span>{iconEllipsis} </span>
               </div>
-              <h1>{data.map(e => e.title === "Study" && e.timeframes.weekly.current)}hrs</h1>
-              <span>Last Week - {data.map(e => e.title === "Study" && e.timeframes.weekly.previous)}hrs</span>
+              <h1>{data.map(e => e.title === "Study" && e.timeframes[dates].current)}hrs</h1>
+              <span>Last Week - {data.map(e => e.title === "Study" && e.timeframes[dates].previous)}hrs</span>
             </div>
           </div>
           <div className="card-exercise">
+            {iconExercise}
             <div className="info">
               <div className="head">
                 <h4>Exercise</h4><span>{iconEllipsis} </span>
               </div>
-              <h1>{data.map(e => e.title === "Exercise" && e.timeframes.weekly.current)}hrs</h1>
-              <span>Last Week - {data.map(e => e.title === "Exercise" && e.timeframes.weekly.previous)}hrs</span>
+              <h1>{data.map(e => e.title === "Exercise" && e.timeframes[dates].current)}hrs</h1>
+              <span>Last Week - {data.map(e => e.title === "Exercise" && e.timeframes[dates].previous)}hrs</span>
             </div>
           </div>
           <div className="card-social">
+            {iconSocial}
             <div className="info">
               <div className="head">
                 <h4>Social</h4><span>{iconEllipsis} </span>
               </div>
-              <h1>{data.map(e => e.title === "Social" && e.timeframes.weekly.current)}hrs</h1>
-              <span>Last Week - {data.map(e => e.title === "Social" && e.timeframes.weekly.previous)}hrs</span>
+              <h1>{data.map(e => e.title === "Social" && e.timeframes[dates].current)}hrs</h1>
+              <span>Last Week - {data.map(e => e.title === "Social" && e.timeframes[dates].previous)}hrs</span>
             </div>
           </div>
           <div className="card-selfcare">
+            {iconSelfCare}
             <div className="info">
               <div className="head">
                 <h4>Self Care</h4><span>{iconEllipsis} </span>
               </div>
-              <h1>{data.map(e => e.title === "Self Care" && e.timeframes.weekly.current)}hrs</h1>
-              <span>Last Week - {data.map(e => e.title === "Self Care" && e.timeframes.weekly.previous)}hrs</span>
+              <h1>{data.map(e => e.title === "Self Care" && e.timeframes[dates].current)}hrs</h1>
+              <span>Last Week - {data.map(e => e.title === "Self Care" && e.timeframes[dates].previous)}hrs</span>
             </div>
           </div>
         </div>
